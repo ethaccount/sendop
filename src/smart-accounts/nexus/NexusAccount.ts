89ce2221122c07f981922857b3730bc5c6b0ef8b
Johnson Chen <chnejohnson@gmail.com>
2025-03-15 21:50:27 +0800

    dev(nexus): implement creation and installModule

--- src/smart-accounts/nexus/NexusAccount.ts
+++ src/smart-accounts/nexus/NexusAccount.ts
@@ -1,7 +1,21 @@
-import type { Bundler, ERC7579Validator, Execution, PaymasterGetter, SendOpResult, UserOp } from '@/core'
+import ADDRESS from '@/addresses'
+import { NexusFactory__factory } from '@/contract-types'
+import {
+	ERC7579_MODULE_TYPE,
+	type Bundler,
+	type ERC7579Validator,
+	type Execution,
+	type PaymasterGetter,
+	type SendOpResult,
+	type UserOp,
+} from '@/core'
+import { SendopError } from '@/error'
+import INTERFACES from '@/interfaces'
+import { abiEncode } from '@/utils'
 import type { JsonRpcProvider } from 'ethers'
+import { concat } from 'ethers/utils'
 import { SmartAccount } from '../SmartAccount'
-import type { NexusCreationOptions } from './types'
+import type { NexusCreationOptions, NexusInstallModuleConfig } from './types'
 
 export type NexusAccountOptions = {
 	address?: string
@@ -39,16 +53,6 @@ export class NexusAccount extends SmartAccount {
 		return this._options.pmGetter
 	}
 
-	// interface
-
-	get interface() {
-		return NexusAccount.interface
-	}
-
-	get factoryInterface() {
-		return NexusAccount.factoryInterface
-	}
-
 	override getSender(): Promise<string> | string {
 		return ''
 	}
@@ -75,26 +79,86 @@ export class NexusAccount extends SmartAccount {
 		return '' as any
 	}
 
-	override getInitCode(creationOptions: any): string {
-		return ''
-	}
-
-	override encodeInitialize(creationOptions: any): string {
-		return ''
-	}
-
-	override encodeInstallModule(config: any): string {
-		return ''
-	}
-
-	static readonly interface = ''
-	static readonly factoryInterface = ''
-
-	static override accountId() {
-		return ''
+	/**
+	 * @returns op.initCode = factory address + factory calldata
+	 */
+	override getInitCode(creationOptions: NexusCreationOptions): string {
+		const factoryCalldata = this.factoryInterface.encodeFunctionData('createAccount', [
+			this.encodeInitialize(creationOptions),
+			creationOptions.salt,
+		])
+		return concat([ADDRESS.NexusFactory, factoryCalldata])
 	}
 
 	static override async getNewAddress(client: JsonRpcProvider, creationOptions: NexusCreationOptions) {
-		return ''
+		const factory = NexusFactory__factory.connect(ADDRESS.NexusFactory, client)
+		const address = await factory.computeAccountAddress(
+			this.encodeInitialize(creationOptions),
+			creationOptions.salt,
+		)
+		return address
+	}
+
+	override encodeInitialize(creationOptions: NexusCreationOptions): string {
+		return NexusAccount.encodeInitialize(creationOptions)
+	}
+	static encodeInitialize(creationOptions: NexusCreationOptions): string {
+		// initializeAccount's initData = abi.encode(bootstrap address, bootstrap calldata)
+		let bootstrapCalldata: string
+		switch (creationOptions.bootstrap) {
+			case 'initNexusWithSingleValidator':
+				bootstrapCalldata = this.bootstrapInterface.encodeFunctionData('initNexusWithSingleValidator', [
+					creationOptions.validatorAddress,
+					creationOptions.validatorInitData,
+					creationOptions.registryAddress,
+					creationOptions.attesters,
+					creationOptions.threshold,
+				])
+				break
+			// TODO: add other bootstrap functions
+			default:
+				throw new NexusError('Unsupported bootstrap function')
+		}
+		const initData = abiEncode(['address', 'bytes'], [ADDRESS.NexusBootstrap, bootstrapCalldata])
+		return this.interface.encodeFunctionData('initializeAccount', [initData])
+	}
+
+	override encodeInstallModule(config: NexusInstallModuleConfig): string {
+		let initData: string
+		switch (config.moduleType) {
+			case ERC7579_MODULE_TYPE.VALIDATOR:
+				initData = config.initData
+				break
+			// TODO: add other module types
+			default:
+				throw new NexusError('Unsupported module type')
+		}
+		return this.interface.encodeFunctionData('installModule', [config.moduleType, config.moduleAddress, initData])
+	}
+
+	static override accountId() {
+		return 'biconomy.nexus.1.0.2'
+	}
+
+	// ================================== interfaces ==================================
+
+	static readonly interface = INTERFACES.Nexus
+	static readonly factoryInterface = INTERFACES.NexusFactory
+	static readonly bootstrapInterface = INTERFACES.NexusBootstrap
+	get interface() {
+		return NexusAccount.interface
+	}
+	get factoryInterface() {
+		return NexusAccount.factoryInterface
+	}
+	get bootstrapInterface() {
+		return NexusAccount.bootstrapInterface
+	}
+}
+
+export class NexusError extends SendopError {
+	constructor(message: string, cause?: Error) {
+		super(message, cause)
+		this.name = 'NexusError'
 	}
 }
--- src/smart-accounts/nexus/types.ts
+++ src/smart-accounts/nexus/types.ts
@@ -1,5 +1,24 @@
-export type NexusCreationOptions = {
+import type { ERC7579_MODULE_TYPE } from '@/core/erc7579'
+
+// TODO:
+export type NexusCreationOptions = SingleValidatorCreation
+
+export type SingleValidatorCreation = {
+	bootstrap: 'initNexusWithSingleValidator'
 	salt: string
 	validatorAddress: string
 	validatorInitData: string
+	registryAddress: string
+	attesters: string[]
+	threshold: number
 }
+
+export type BaseNexusInstallModuleConfig<T extends ERC7579_MODULE_TYPE> = {
+	moduleType: T
+	moduleAddress: string
+	initData: string
+}
+
+export type ValidatorNexusInstallModuleConfig = BaseNexusInstallModuleConfig<ERC7579_MODULE_TYPE.VALIDATOR>
+
+export type NexusInstallModuleConfig = ValidatorNexusInstallModuleConfig
