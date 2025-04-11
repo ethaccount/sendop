cfb6c9180750de3b5d32d5a4b766b92440520f3f
Johnson Chen <chnejohnson@gmail.com>
2025-04-11 20:32:24 +0800

    feat: add PublicPaymaster contract and deployment script

---
+++ contracts/script/deployPublicPaymaster.s.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {PublicPaymaster} from "../src/PublicPaymaster.sol";
+
+/*
+cd contracts
+
+// deploy on sepolia
+forge script script/deployPublicPaymaster.s.sol --rpc-url $sepolia --broadcast --verify
+
+// verify on sepolia
+forge verify-contract \                                                           17s
+    --chain-id 11155111 \
+    --compiler-version v0.8.28+commit.8e94f5c9 \
+    --watch \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
+    0xcb04730b8aA92B8fC0d1482A0a7BD3420104556D \
+    PublicPaymaster
+
+// deposit
+cast send --account dev  --rpc-url $sepolia \
+    $ep8 \
+    "depositTo(address account)" \
+    0xcb04730b8aA92B8fC0d1482A0a7BD3420104556D \
+    --value 0.5ether
+
+*/
+
+contract DeployPublicPaymaster is Script {
+    function setUp() public {}
+
+    function run() public {
+        bytes32 salt = vm.envBytes32("SALT");
+        console.logBytes32(salt);
+
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(privateKey);
+
+        PublicPaymaster publicPaymaster = new PublicPaymaster{salt: salt}();
+
+        console.log("PublicPaymaster at:", address(publicPaymaster));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ contracts/src/PublicPaymaster.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "aa-0.8/contracts/interfaces/IPaymaster.sol";
+
+contract PublicPaymaster is IPaymaster {
+    function validatePaymasterUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
+        external
+        returns (bytes memory context, uint256 validationData)
+    {
+        return ("", 0);
+    }
+
+    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost, uint256 actualUserOpFeePerGas)
+        external
+    {}
+}
