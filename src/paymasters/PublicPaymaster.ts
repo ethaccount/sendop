160c327a6075d915536723eb4f279bce4e483b09
Johnson Chen <chnejohnson@gmail.com>
2025-04-11 20:45:52 +0800

    feat: add PublicPaymaster

---
+++ src/paymasters/PublicPaymaster.ts
@@ -0,0 +1,20 @@
+import type { GetPaymasterStubDataResult, PaymasterGetter, UserOp } from '@/core'
+import { toBeHex } from 'ethers'
+
+export class PublicPaymaster implements PaymasterGetter {
+	public address: string
+
+	constructor(address: string) {
+		this.address = address
+	}
+
+	async getPaymasterStubData(_userOp: UserOp): Promise<GetPaymasterStubDataResult> {
+		return {
+			paymaster: this.address,
+			paymasterData: '0x',
+			paymasterVerificationGasLimit: toBeHex(999_999n),
+			paymasterPostOpGasLimit: toBeHex(999_999n),
+			isFinal: true,
+		}
+	}
+}
--- src/paymasters/index.ts
+++ src/paymasters/index.ts
@@ -1,1 +1,2 @@
 export * from './PimlicoPaymaster'
+export * from './PublicPaymaster'
