ed029bd91d326f4ce5eb80e172143329afec6359
Johnson Chen <chnejohnson@gmail.com>
2025-04-30 10:57:35 +0800

    v0.4.2-beta.2

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -5,6 +5,7 @@ import {
 	AbiCoder,
 	concat,
 	dataSlice,
+	getAddress,
 	isAddress,
 	keccak256,
 	toBeHex,
@@ -191,4 +192,12 @@ export async function isSmartEOA(client: JsonRpcProvider, address: string) {
 		return true
 	}
 	return false
+}
+
+export async function getSmartEOADelegateAddress(client: JsonRpcProvider, address: string): Promise<string | null> {
+	const code = await client.getCode(address)
+	if (code.startsWith('0xef0100') && isAddress(dataSlice(code, 3, 23)) && getBytesLength(code) === 23) {
+		return getAddress(dataSlice(code, 3, 23))
+	}
+	return null
 }
